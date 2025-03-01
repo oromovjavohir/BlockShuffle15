0c5e21e59b31688e32f98ee40df71cf11b40a9f0
Javohir <oromovjavoxir@gmail.com>
2025-03-02 00:11:48 +0500

    delete a file

--- app/src/main/java/com/example/blockshuffle15/MainActivity.kt
+++ app/src/main/java/com/example/blockshuffle15/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.blockshuffle15
-
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
