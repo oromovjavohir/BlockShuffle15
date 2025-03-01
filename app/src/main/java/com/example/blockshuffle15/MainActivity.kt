e68d81d5b7bb3ee86156a74fa1167545ca5248d5
Javohir <oromovjavoxir@gmail.com>
2025-03-02 00:15:38 +0500

    delete a file

--- app/src/main/java/com/example/blockshuffle15/MainActivity.kt
+++ app/src/main/java/com/example/blockshuffle15/MainActivity.kt
@@ -1,12 +1,12 @@
-//package com.example.blockshuffle15
-//import android.os.Bundle
-//import androidx.appcompat.app.AppCompatActivity
-//class MainActivity : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//    }
-//}
+package com.example.blockshuffle15
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
 private fun init(){
 
 }
